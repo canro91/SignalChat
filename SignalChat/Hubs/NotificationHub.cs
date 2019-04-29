87189dee589021b397e449fa6853a5b7cb622b1e
canro91 <canro91@hotmail.com>
2019-04-29 17:11:40 -0500

    Remove dead code on hub

--- SignalChat/Hubs/NotificationHub.cs
+++ SignalChat/Hubs/NotificationHub.cs
@@ -9,18 +9,14 @@ namespace SignalChat.Hubs
     {
         public override Task OnConnected()
         {
-            // 1. Add to group
             if (Context.User?.Identity?.IsAuthenticated == false)
                 throw new System.Exception("Usted no pasara mas alla de este nivel");
 
-            // 2. Add to connected
             var claims = (Context.User.Identity as ClaimsIdentity).Claims;
             var username = claims.FirstOrDefault(c => c.Type == ClaimTypes.Name).Value;
 
-            // 3. Update user count
             Clients.All.updateUsers(username);
 
-            // 4. Welcome message
             Clients.Client(Context.ConnectionId).broadcastMessage(
                 "Welcome to the chat room, " + username, "[server]");
 
@@ -35,28 +31,5 @@ namespace SignalChat.Hubs
 
             Clients.All.broadcastMessage(message, username);
         }
-
-        public override Task OnDisconnected(bool stopCalled)
-        {
-            // 1. Remove from group
-            //Groups.Remove(Context.ConnectionId, "authenticated");
-
-            // 2. Remove from connected
-            //var disconnected = ConnectedClients.FirstOrDefault(t => t.ID == Context.ConnectionId);
-            //ConnectedClients.Remove(disconnected);
-
-            // 3. Update user count
-            //var count = ConnectedClients.Count;
-            //var allUsernames = ConnectedClients.Select(t => t.Username).ToList();
-            //Clients.All.updateUsers(count, allUsernames);
-
-            return base.OnDisconnected(stopCalled);
-        }
-    }
-
-    public class Client
-    {
-        public string Username { get; set; }
-        public string ID { get; set; }
     }
 }
