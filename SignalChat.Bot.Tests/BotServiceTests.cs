d9452d67f1a36354bb4544373f2de119ed71a888
canro91 <canro91@hotmail.com>
2019-04-30 13:36:44 -0500

    Add test for bot functionality

---
+++ SignalChat.Bot.Tests/BotServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Foundatio.Queues;
+using NUnit.Framework;
+using SignalChat.Bot.Contracts;
+using SignalChat.Core.Domain;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalChat.Bot.Tests
+{
+    [TestFixture]
+    public class BotServiceTests
+    {
+        [Test]
+        public async Task BotService_AGivenCommand_EnqueueMessage()
+        {
+            var stock = @"Symbol,Date,Time,Open,High,Low,Close,Volume
+AAPL.US,2019-04-26,22:00:20,204.9,205,202.12,204.3,18649102";
+            var fakeStockService = new FakeStockService(stock);
+            var messageQueue = new InMemoryQueue<Message>();
+            var botService = new BotService(fakeStockService, messageQueue);
+
+            await botService.QueryAndSend(stockCode: "aapl.us");
+
+            var lastMessage = await messageQueue.DequeueAsync();
+            Assert.IsNotNull(lastMessage);
+        }
+
+    }
+
+    public class FakeStockService : IStockService
+    {
+        public string Stock;
+
+        public FakeStockService(string stock)
+        {
+            this.Stock = stock;
+        }
+
+        public byte[] FindStockQuote(string stockCode)
+        {
+            return Encoding.UTF8.GetBytes(Stock);
+        }
+    }
+}
